25c0c2e3f43b5dc65c8bc7d7a930f40b8c422f55
danishkhan8055 <danish.k88274@gmail.com>
2025-05-01 12:21:23 +0530

    for deployment

--- UI/src/apiURLs.js
+++ UI/src/apiURLs.js
@@ -1,9 +1,9 @@
-export const _apiurluser="http://localhost:3001/user/";
+export const _apiurluser=`${process.env.REACT_APP_API_URL}/user/`;
 
-export const _apiurlcategory="http://localhost:3001/category/";
+export const _apiurlcategory=`${process.env.REACT_APP_API_URL}category/`;
 
-export const _apiurlsubcategory="http://localhost:3001/subcategory/";
+export const _apiurlsubcategory=`${process.env.REACT_APP_API_URL}subcategory/`;
 
-export const _apiurlproduct="http://localhost:3001/product/";
+export const _apiurlproduct=`${process.env.REACT_APP_API_URL}product/`;
 
-export const _apiurlbid="http://localhost:3001/bid/";
+export const _apiurlbid=`${process.env.REACT_APP_API_URL}bid/`;
