f879c3663bac370c5a372e8b9e0d6db67d089dc6
danishkhan8055 <danish.k88274@gmail.com>
2025-05-01 15:45:56 +0530

    finally

--- UI/src/apiURLs.js
+++ UI/src/apiURLs.js
@@ -1,9 +1,9 @@
 export const _apiurluser=`${process.env.REACT_APP_API_URL}/user/`;
 
-export const _apiurlcategory=`${process.env.REACT_APP_API_URL}category/`;
+export const _apiurlcategory=`${process.env.REACT_APP_API_URL}/category/`;
 
-export const _apiurlsubcategory=`${process.env.REACT_APP_API_URL}subcategory/`;
+export const _apiurlsubcategory=`${process.env.REACT_APP_API_URL}/subcategory/`;
 
-export const _apiurlproduct=`${process.env.REACT_APP_API_URL}product/`;
+export const _apiurlproduct=`${process.env.REACT_APP_API_URL}/product/`;
 
-export const _apiurlbid=`${process.env.REACT_APP_API_URL}bid/`;
+export const _apiurlbid=`${process.env.REACT_APP_API_URL}/bid/`;
